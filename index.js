22f136bcd3e0c8897d927f4114e5582e13f07ca9
kurapoko <kuraharamasahiro@gmail.com>
2020-08-17 01:01:37 +0900

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const jsonString = `
+{
+  "students": 40,
+  "grade": 4,
+  "name": "c組"
+}
+`;
+
+const data = JSON.parse(jsonString);
+appDiv.innerHTML+=data.name;
+appDiv.innerHTML+=data.grade;
+
+const myArray = ['鈴木', '田中', '高橋', '後藤'];
+
+const targetUser = myArray.find((element) => element === '田中');
+
+appDiv.innerHTML+=targetUser;
+
